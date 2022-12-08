8b6ad6532de595a940943a6813d0d69709d40ea7
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-08 14:04:09 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-/* this code outputs the words inputted */
-int main (void)
+/**
+*
+*main- print the inputted line
+*
+*return 0
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
