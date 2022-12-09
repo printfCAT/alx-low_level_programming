c6dea7f766bdd947bcb4db60b940a7b9556cb8f2
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-09 13:07:13 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+		printf("\n");
+	return (0);
+}
