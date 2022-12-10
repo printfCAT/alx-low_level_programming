d48ec90adcbb6f5c048cab083f164d5a1ce3329c
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-10 13:44:03 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,12 @@ int main(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
-		if (c == 9)
-			continue;
+	if (c != '9')
+	{
 
 		putchar(',');
 		putchar(' ');
+	}
 	}
 		putchar('\n');
 	return (0);
