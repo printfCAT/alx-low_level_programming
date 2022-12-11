f0213f0c4ca082e34b002acdfd1df0723fe4ff4c
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-11 10:10:13 -0800

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - Entry main
+*
+* Return: 0
+*/
+int main(void)
+{
+	int c;
+	int i;
+	int a;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = c + 1; i <= '9'; i++)
+		{
+			for (a = i + 1; a <= '9'; a++)
+			{
+				putchar(c);
+				putchar(i);
+				putchar(a);
+			if (a + 8 != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
