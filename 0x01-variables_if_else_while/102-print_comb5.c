513fb27e06fe38a05440a43d2d07c13cd897579a
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-12 00:25:14 -0800

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int a, b, c, i;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; i++)
+			{
+				for (i = '1'; i <= '9'; i++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(i);
+					if ((a == '9') && (b == '8') && (c == '9') && (i == '9'))
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
