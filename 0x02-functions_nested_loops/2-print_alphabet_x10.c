47f8b6f4eecbcc36b363688800ffdd3a04c77fd0
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 13:59:07 -0800

    print alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	int j;
+	int count = 0;
 
-		while (j = 0; j <= 9; j++)
+		while (count++ <= 9)
 		{
 			for (i = 'a'; i <= 'b'; i++)
 			{
-				_putchar(j);
+				_putchar(count);
 			}
 			_putchar('\n');
 		}
