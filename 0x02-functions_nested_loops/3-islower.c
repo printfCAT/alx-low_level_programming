e71e8eae11865bc887e576c9ecbbbd797941a564
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 14:25:38 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _islower - checks for lowercase character
+*
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	if((c >= 'a') || (c <= 'z'))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
