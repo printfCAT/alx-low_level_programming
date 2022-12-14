0d69eff91078b2b2ab41b609c3edd0743ed53298
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 12:25:53 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - Prints alphabet in lower case
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'b'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
