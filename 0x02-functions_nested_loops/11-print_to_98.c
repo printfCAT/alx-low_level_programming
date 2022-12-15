789de3748a953447b99ad1019f4e8495be8abf33
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 17:54:13 -0800

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - prints all natural numbers up to 98
+*/
+void print_to_98(int n);
+{
+	int i;
+
+	if (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+	}
+		int n;
+
+		if (n = 48; n <= 56; n++)
+		{
+			putchar(i);
+			putchar(n);
+			putchar(',');
+		}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _abs(int x);
 int print_last_digit(int c);
 void jack_bauer(void);
 int add(int a, int b);
+void print_to_98(int n);
