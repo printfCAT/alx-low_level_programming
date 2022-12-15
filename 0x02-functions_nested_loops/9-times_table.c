f70694685d486c99faf0961ca335d5c113c0a23b
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 18:42:21 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+* times_table - prints the 9 multiplication table
+*
+*/
+void times_table(void)
+{
+	int s =0;
+	int e = 9;
+	int count = 0;
+
+	while (count <= 9)
+	{
+		while (s <= e)
+		{
+			int product = count * s;
+
+			if (s != 0)
+			{
+				_putchar(',');
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+					_putchar(' ');
+			}
+
+			if (product < 10)
+				_putchar(prodcut + '0');
+			else
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+
+			if (s == e)
+				_putchar('\n');
+			s++
+		}
+		s = 0;
+		count++;
+	}
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int x);
 int print_last_digit(int c);
+void times_tables(void);
 void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
