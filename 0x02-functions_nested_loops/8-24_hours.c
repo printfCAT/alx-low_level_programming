42df12352fafccc0727a7fde52642c2b18b10be5
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 17:02:41 -0800

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* jack_bauer - prints every minute of the day of jack bauer
+*
+* Return: 0
+*/
+void jack_bauer(void)
+{
+	int a,b,c,i;
+
+	for (a = '0'; a <= '2'; a++)
+	{
+		for (b = '0'; b <= '3'; b++)
+		{
+			for (c = '0'; c <= '5'; c++)
+			{
+				for (i = '0'; i <= '9'; i++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(i);
+				}
+			}
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
