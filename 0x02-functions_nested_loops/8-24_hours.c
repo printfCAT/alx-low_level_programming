4d5e7e9c711aeabad87ae5c3df14c39546569771
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 17:18:04 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,7 +22,7 @@ void jack_bauer(void)
 					_putchar(i);
 					_putchar('\n');
 					if ((a == '2') && (b == '3') && (c == '5') && (i == '9'))
-						return (0);
+						exit(EXIT_SUCCESS);
 				}
 			}
 		}
