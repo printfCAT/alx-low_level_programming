fe8fabd28d1333df003434435d57e9ccd29f647e
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 18:45:24 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 			}
 
 			if (product < 10)
-				_putchar(prodcut + '0');
+				_putchar(product + '0');
 			else
 			{
 				_putchar(product / 10 + '0');
@@ -37,7 +37,7 @@ void times_table(void)
 
 			if (s == e)
 				_putchar('\n');
-			s++
+			s++;
 		}
 		s = 0;
 		count++;
