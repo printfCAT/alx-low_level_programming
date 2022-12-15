42d99e73bdc756b2d8b52b87e24a1528d63f56d0
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-14 18:25:29 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 		while (n >= 98)
 			printf("%d,", n--);
 		if (n != 98)
-			printf(", ", n);
+			printf(", ");
 	}
 
 	else
@@ -19,6 +19,6 @@ void print_to_98(int n)
 		while (n <= 98)
 			printf("%d, ", n++);
 		if (n != 98)
-			printf(", ", n);
+			printf(", ");
 	}
 }
