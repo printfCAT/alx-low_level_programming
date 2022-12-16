4d2b52a6784c59396275c3471d6842e6be452604
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-16 09:52:47 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,27 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
-		}
-		else if ((i % 3) == 0 && (i % 5) == 0)
-		{
-			printf("FizzBuzz");
-		}
+		}		
 		else
 		{
 			printf("%d", i);
 		}
-		if (i != 100)
+		if (i == 100)
 		{
-			printf(" ");
+			continue;
 		}
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
