f4e8a3abb4a29581a56b47389340f909e222d32d
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-16 07:40:45 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,14 @@ void more_numbers(void)
 	int c;
 	int count = 0;
 
-	while (count++ <= 9)
+	for (count = 0; count <= 9; count++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			_putchar((c / 10) + '0');
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+			}
 			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
