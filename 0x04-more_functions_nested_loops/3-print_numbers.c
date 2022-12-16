dca118f2c6960cbe768273cb747854b77ca03929
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-16 06:01:36 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - prints numbers from 0 to 9
+* Return: 0
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9')
+	{
+		_putchar(i);
+		i++
+	}
+	_putchar('\n');
+	return (0);
+}
