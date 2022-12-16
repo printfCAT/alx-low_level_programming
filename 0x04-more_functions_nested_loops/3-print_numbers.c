0f6e33e22f225e744b76132444f8e801788f082c
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-16 06:06:25 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
 * print_numbers - prints numbers from 0 to 9
+* _putchar - function for printing
 * Return: 0
 */
 void print_numbers(void)
+void _putchar(int c)
 {
-	int i;
+	int c;
 
-	for (i = '0'; i <= '9')
+	for (c = '0'; c <= '9')
 	{
-		_putchar(i);
-		i++
+		_putchar(c);
+		c++
 	}
 	_putchar('\n');
 	return (0);
