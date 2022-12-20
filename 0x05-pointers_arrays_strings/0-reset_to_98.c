8295808623dd2c97164e099ff64d573af291fb9d
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-20 03:56:49 -0800

    pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* reset_to_98 - replaces value using pointer
+*@n: variable whose value is being updated
+*
+* Return: 0
+*/
+void reset_to_98(int *n)
+{
+	int i = 402;
+
+	n = &i;
+	*n = 98;
+	_putchar(i);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putchar(int n);
+void reset_to_98(int *n);
