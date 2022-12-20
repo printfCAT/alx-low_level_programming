05741e191dea11f451c0535768fd1406b0739514
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-20 04:49:58 -0800

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+* @s: string whose lenght is being checked
+*
+* Return: lenghth of string
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	return (len);
+}
