d2c36a22e006f7845490f0d9fbc5de6aaf08a62d
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-20 10:30:31 -0800

    reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* rev_string - reverses a string
+* @s: sting being reversed
+*
+* Return: nothing
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		s++;
+		i++;
+	}
+	while (i)
+	{
+		s--;
+		i--;
+	}
+}
