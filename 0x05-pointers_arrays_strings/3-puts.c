a77bfac4c677a4d657f6fe8380a0470cd473a045
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-20 05:26:54 -0800

    prints a string then newline then adds to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,9 +3,10 @@
 * _puts - prints a string followed by a newline to stdout
 * @str: string being written
 *
-* Return: nothing
+* Return: 0
 */
 void _puts(char *str)
 {
 	_puts(str);
+	return (0);
 }
