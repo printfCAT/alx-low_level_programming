2edd9c13f1a170a03a2387b037ff67902205aa77
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-21 23:43:01 -0800

    working with more pointers arrays & strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _strcat - concatenates 2 strings
+* @dest: string 1
+* @src: string 2
+*
+* Return: pointer to the resulting string
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+	int i = 0, c = 0, len;
+
+	while (*dest)
+	{
+		dest++;
+		c++;
+	}
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		dest[c + 1] = src[i];
+		c++;
+		src++;
+	}
+	*ptr = dest[c + 1] = src[i];
+	return (ptr);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+working with more pointers, arrays & strings
