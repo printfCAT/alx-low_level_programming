20ac1219950c33f2dd61cabe0e550303bea05036
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-23 15:31:54 -0800

    changes all lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase
+*
+* Return: uppercased string
+*/
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] -= 32;
+	}
+	return (c);
+}
