b18313d62ec80283df7b81652abcd9bce13a0216
RIvAL-411 <cliffomanyo@gmail.com>
2022-12-23 16:49:52 -0800

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* cap_string - capitalizes all words of a string
+* @c: string to be capitalized
+*
+* Return: capitalized string
+*/
+char *cap_string(char *c)
+{
+	int i;
+
+	while (c[i])
+	{
+		while (!(c[i] >= 'a' && c[i] <= 'z'))
+			i++;
+		{
+		if (c[i - 1] == ' ' || c[i - 1] == '\t' || c[i - 1] == '\n'
+		|| c[i - 1] == ',' || c[i - 1] == ';' || c[i - 1] == '.' ||
+		c[i - 1] == '!' || c[i - 1] == '?' || c[i - 1] == '"' ||
+		c[i - 1] == '(' || c[i - 1] == ')' || c[i - 1] == '{' ||
+		c[i - 1] == '}' || i == 0)
+			c[i] -= 32;
+		i++;
+		}
+	}
+	return (c);
+}
