d29921f66ad48ea98da0483b288614eaaf4b47ff
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-05 05:22:26 -0800

    print string plus newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts_recursion - prints a string followed by newline
+* @s: string to be printed
+*
+* return: string
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
