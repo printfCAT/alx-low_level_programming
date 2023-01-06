07e8877b746c8e0932c56eed9a9580344703fb55
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-06 13:49:32 -0800

    find prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+* support - finds prime number
+* @a: number we are checking
+* @b: number we are using to check
+*
+* Return: prime number
+*/
+int support(int a, int b)
+{
+	if (b  == a - 1)
+		return (1);
+	else if ((a % b) == 0)
+		return (0);
+	if ((a % b) != 0)
+		return (support(a, b + 1));
+	return (0);
+}
+
+/**
 * is_prime_number - looks for prime numbers
 * @n: integer to be checked
 *
@@ -7,9 +25,9 @@
 */
 int is_prime_number(int n)
 {
+	if (n < 2)
+		return (0);
 	if (n == 2 || n == 3)
 		return (1);
-	if (n % 2 == 0)
-		return (0);
-	return (is_prime_number(n + 1));
+	return (support(n, 2));
 }
