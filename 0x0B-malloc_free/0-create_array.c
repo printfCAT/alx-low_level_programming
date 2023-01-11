668b11f928eb017fe17b7b0cc4a3ede4c2d80edf
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-11 03:35:16 -0800

    create & initialize

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,8 @@ char *create_array(unsigned int size, char c)
 	if (s == 0)
 		return ('\0');
 	ca = malloc(s * sizeof(char));
+	if (s == 0)
+		return ('\0');
 	for (i = 0; i < s; i++)
 		ca[i] = c;
 	return (ca);
