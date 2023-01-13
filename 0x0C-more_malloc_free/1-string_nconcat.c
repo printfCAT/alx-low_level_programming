693cda207154bb77ba057c42097815bb9fa852d5
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-13 13:36:03 -0800

    concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -46,11 +46,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *c;
 
 	k = n;
-	size = _strlen(s1) + _strnlen(s2, n) + 1;
+	size = _strlen(s1) + _strnlen(s2, n);
 	c = malloc(sizeof(char) * size);
 	if (c == 0)
 		return (NULL);
-	for (i = 0; s1[i]  != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		c[i] = s1[i];
 	for (j = 0; s2[j] != '\0' && j < k; j++)
 	{
