807c1288a3b831fe7f3099c0e15b235654f6ab15
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-13 14:26:52 -0800

    allocates memory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - allocates memory and initializes it to 0
+* @nmemb: number of elements
+* @size: size to allocate
+*
+* Return: pointer to allocated memory. NULL otherwise
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *c;
+
+	if (size == 0 && nmemb == 0)
+		return (NULL);
+	c = malloc(size * sizeof(char));
+	if (c == 0)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		c[i] = 0;
+	return (c);
+}
