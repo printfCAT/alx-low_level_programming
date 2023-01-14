05a323d693fa693ea5e01fa92a13dd414bccf3a4
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-13 16:45:14 -0800

    array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = malloc(sizeof(*size) * ((max - min) + 1));
-	if (size == 0)
+	if (size == NULL)
 		return (NULL);
 	for (i = 0; i <= max; i++)
 	{
