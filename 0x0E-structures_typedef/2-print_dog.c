ce33879bf8963023e771cc92d1affd8fad94b18d
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-17 15:04:19 -0800

    prints a type dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)\n Age: %.6fOwner: %s\n", d->age, d->owner);
-		else if (d->age == 0)
+		else if (!(d->age))
 			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
 		else if (d->owner == NULL)
 			printf("Name: %s\n Age: %.6f\nOwner: (nil)\n", d->name, d->age);
