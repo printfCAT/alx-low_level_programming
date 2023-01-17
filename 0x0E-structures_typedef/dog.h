9374dbc25659e08005275ad7bcdec02f1ff25faa
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-17 11:21:06 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog - defines elements for dog
+* @name: name of dog
+* @age: age of dog
+* @owner: name of dog owner
+*
+* Description: provides details about the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
