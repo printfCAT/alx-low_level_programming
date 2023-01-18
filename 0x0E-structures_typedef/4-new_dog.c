426b71a15487b7287a777a617ba79b302f4a862e
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-17 17:57:18 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,21 +43,21 @@ char *_strcpy(char *dest, char *src)
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *new;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == 0)
+	new = malloc(sizeof(dog_t));
+	if (new == 0)
 		return (NULL);
-	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (new_dog->name == 0)
+	new->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (new->name == 0)
 		return (NULL);
-	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (new_dog->owner == 0)
+	new->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new->owner == 0)
 		return (NULL);
-	new_dog->name = _strcpy(new_dog->name, name);
-	new_dog->age = age;
-	new_dog->owner = _strcpy(new_dog->owner, owner);
-	return (new_dog);
+	new->name = _strcpy(new->name, name);
+	new->age = age;
+	new->owner = _strcpy(new->owner, owner);
+	return (new);
 }
