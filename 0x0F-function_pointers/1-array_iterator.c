0293a41cd1f7e0a016aa92e9d7ebc62cd88bdf80
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-19 14:10:13 -0800

    iterate array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* array_iterator - executes a function on each element of an array
+* @array: array of elements
+* @size: size of array
+* @action: pointer to function
+*
+* Return: nothing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (array && size)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 
 void print_name(char *name, void (*f)(char *));
 int _putchar(int n);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
