ea8d807a02216e273752a02727b17d2c9a00efc0
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-21 06:27:37 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_doggo;
-	unsigned long int i;
+	int i, len_name, len_owner;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
@@ -22,25 +22,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_doggo);
 		return (NULL);
 	}
-	new_doggo->name = malloc(strlen(name) + 1);
+	len_name = strlen(name);
+	new_doggo->name = malloc(len_name + 1);
 	if (new_doggo->name == NULL)
 	{
 		free(new_doggo);
 		free(new_doggo->name);
 		return (NULL);
 	}
-	new_doggo->owner = malloc(strlen(owner) + 1);
+	len_owner = strlen(owner);
+	new_doggo->owner = malloc(len_owner + 1);
 	if (new_doggo->owner == NULL)
 	{
 		free(new_doggo);
 		free(new_doggo->owner);
 		return (NULL);
 	}
-	for (i = 0; i < strlen(name); i++)
+	for (i = 0; i < len_name; i++)
 		new_doggo->name[i] = name[i];
 	new_doggo->name[i] = '\0';
 	new_doggo->age = age;
-	for (i = 0; i < strlen(owner); i++)
+	for (i = 0; i < len_owner; i++)
 		new_doggo->owner[i] = owner[i];
 	new_doggo->owner[i] = '\0';
 
