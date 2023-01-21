933aef07a68954922e86873c04baee3fe1a0bd4a
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-21 06:32:31 -0800

    changes to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG
 #define DOG
 
+/**
+* dog_t - alias for struct dog
+*
+*/
+typedef struct dog dog_t;
 
 /**
 * struct dog - defines elements for dog
@@ -18,7 +23,7 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
