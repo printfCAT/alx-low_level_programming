2d62e6a63d97aebcf054eb73c70b9ed03f9e9203
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-22 11:25:02 -0800

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	operator = *argv[2];
 
-	if ((operator == '/' || operator == '%') && num2 == 0) 
+	if ((operator == '/' || operator == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
