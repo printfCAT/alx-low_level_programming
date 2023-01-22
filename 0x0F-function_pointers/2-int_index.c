ea95693590ad5ee203eff4ba81ebf6110ed225da
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-22 04:49:30 -0800

    search for integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -21,9 +21,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 		for (i = 0; i < size; i++)
 		{
 			cmp(array[i]);
-			if (cmp(array[i]) == 0)
-				return (-1);
+			if (cmp(array[i]) != 0)
+				return (i);
 		}
 	}
-	return (i);
+	return (-1);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
+#include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-int int_index(int *array, int size, int (*cmp)(int));
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
