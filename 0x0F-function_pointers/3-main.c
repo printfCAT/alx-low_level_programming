eff0f9d36486917c09583c16ad437b459f34c04a
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-22 11:22:56 -0800

    simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - performs simple operations
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: result of operation. NULL otherwise
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2, calc;
+	char operator;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = get_op_func(argv[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = *argv[2];
+
+	if ((operator == '/' || operator == '%') && num2 == 0) 
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	calc = op(num1, num2);
+
+	printf("%d\n", calc);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+* op_add - adds 2 integers
+* @a: 1st integer
+* @b: 2nd integer
+*
+* Return: sum
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - subtracts 2 integers
+* @a: 1st integer
+* @b: 2nd integer
+*
+* Return: subtraction
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiplies 2 integers
+* @a: 1st integer
+* @b: 2nd integer
+*
+* Return: multiplication
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - divides 2 integers
+* @a: 1st integer
+* @b: 2nd integer
+*
+* Return: division
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - finds modulus of 2 integers
+* @a: 1st integer
+* @b: 2nd integer
+*
+* Return: modulus
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
