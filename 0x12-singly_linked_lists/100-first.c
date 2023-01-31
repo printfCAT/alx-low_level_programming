a6549d84f4e9e54d4fdc3b0cc30fc43210729bfd
RIvAL-411 <cliffomanyo@gmail.com>
2023-01-31 11:43:08 -0800

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* before_main - prints a string before the main function
+*
+* Returns: nothing
+*/
+void before_main(void)__attribute__((constructor));
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
