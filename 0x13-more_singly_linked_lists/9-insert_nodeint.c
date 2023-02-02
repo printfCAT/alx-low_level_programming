160d9ce4d818d0cc99bf2138da9a534a71d5ec5b
RIvAL-411 <cliffomanyo@gmail.com>
2023-02-02 02:48:09 -0800

    linked lists op

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *new, *temp;
 
-	if (!(*head))
+	if (!(head))
 		return (NULL);
 	if (idx != 0)
 	{
