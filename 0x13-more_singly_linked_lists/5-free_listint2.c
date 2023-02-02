dc9c7dfd9b490602806b31da9671b9a819a4c8cf
RIvAL-411 <cliffomanyo@gmail.com>
2023-02-02 02:40:45 -0800

    linked lists op

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
+	if (!(head))
+		return;
 	while (*head)
 	{
 		current = (*head)->next;
 		free(*head);
 		*head = current;
 	}
-	*head = NULL;
+	head = NULL;
 }
