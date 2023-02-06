f19de4d4647c0bf550a4ab581f02f4e030c34bb5
RIvAL-411 <cliffomanyo@gmail.com>
2023-02-06 10:59:38 -0800

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,11 +8,13 @@
 */
 void print_binary(unsigned long int n)
 {
-	int i;
+	int i, temp = n;
 
-	for (i = 31; i >= 0; i--)
+	for ( i = 0; (temp >>= 1) > 0; i++)
+		;
+	for (; i >= 0; i--)
 	{
-		if (n & (1 << i))
+		if ((n >> i) & 1)
 			printf("1");
 		else
 			printf("0");
