6a0de79254a6076f48719f1682c437564f5b4ac8
RIvAL-411 <cliffomanyo@gmail.com>
2023-02-06 18:57:12 -0800

    get bitindex

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* get_bit - returns balue of a bit at a given index
+* @n: an unsigned long int
+* @index: position of bit
+*
+* Return: value of int. -1 if unsuccessful
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	i = (n & (1 << index)) != 0;
+
+	return (i);
+}
