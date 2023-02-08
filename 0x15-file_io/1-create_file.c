4e79eabd17a733d767b199fd5b0d5706838bc7c8
RIvAL-411 <cliffomanyo@gmail.com>
2023-02-08 12:40:44 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int ptr, wrt;
 
-	if (!text_content)
+	if (text_content == NULL)
 	{
 		ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		if (ptr == -1 || filename == NULL)
