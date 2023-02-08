a02cc4e6077e82408700f1009d408a3841903b3e
RIvAL-411 <cliffomanyo@gmail.com>
2023-02-08 09:42:45 -0800

    read & print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -39,6 +39,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
+	if (ferror(ptr))
+	{
+		free(ch);
+		return (0);
+	}
+
 	free(ch);
 	fclose(ptr);
 	return (wrt);
