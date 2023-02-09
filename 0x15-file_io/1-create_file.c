3c79c51496ed3a2eaa65460cc8d67f3445642326
RIvAL-411 <cliffomanyo@gmail.com>
2023-02-08 18:26:17 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,9 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
+	text_content = malloc(sizeof(char) * (len - 1));
+	if (text_content == NULL)
+		return (-1);
 
 	ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(ptr, text_content, len);
