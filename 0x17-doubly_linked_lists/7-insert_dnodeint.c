6e6fa94261d3f93750e8d13ee6ab99e94e361337
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-18 04:17:29 -0700

    insert node index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position in a doubly linked list
+* insert_dnodeint_at_index - inserts a new node at a given position
+* in a doubly linked list
 * @h: 1st node of the linked list
 * @idx: position to insert new node
 * @n: data for the new node
@@ -12,6 +13,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *new, *temp = *h;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	new = malloc(sizeof(dlistint_t));
 	if (!*h || !new)
 		return (NULL);
@@ -21,11 +24,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 		if (!temp)
+		{
+			free(new);
 			return (NULL);
+		}
 	}
 	new->next = temp->next;
+	if (temp->next)
+		temp->next->prev = new;
 	new->prev = temp;
 	temp->next = new;
 
-	return(new);
+	return (new);
 }
