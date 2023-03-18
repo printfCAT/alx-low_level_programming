b8131792d5b7152be7ea0f4122b1c8471a3b0c22
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-18 08:58:17 -0700

    delete node index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,29 +9,32 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	dlistint_t *temp = *head;
+	dlistint_t *tmp = *head;
 
-	if (!*head)
+	if (*head == NULL)
 		return (-1);
-	if (index == 0)
-	{
-		temp = temp->next;
-		free(*head);
-		*head = temp;
-		return (1);
-	}
-	while (i < index)
-	{
-		temp = temp->next;
-		i++;
-		if (!temp)
-			return (-1);
-	}
-	temp->prev->next = temp->next;
-	if (temp->next)
-		temp->next->prev = temp->prev;
-	free(temp);
 
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
+
+	free(tmp);
 	return (1);
 }
