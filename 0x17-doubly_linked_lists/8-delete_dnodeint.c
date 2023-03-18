28592dd1cda3044f1243eaf94065659b7e63d1a9
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-18 08:39:41 -0700

    delete node index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,6 +21,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = temp;
 		return (1);
 	}
+	if (!temp-next)
+		return (-1);
 	while (i < index - 1)
 	{
 		temp = temp->next;
