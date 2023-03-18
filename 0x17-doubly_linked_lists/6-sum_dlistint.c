9de0442948bd6f23bb0c08890f72b2b2fde903d2
RIvAL-411 <cliffomanyo@gmail.com>
2023-03-18 03:39:45 -0700

    sum of data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+* sum_dlistint_t - returns the sum of all data of a doubly linked list
+* @head: 1st node of the linked list
+*
+* Return: sum of all data. 0 if list is empty
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int add = 0;
+	dlistint_t *temp = head;
+
+	if (!head)
+		return (0);
+	while (temp)
+	{
+		add += temp->n;
+		temp = temp->next;
+	}
+	return (add);
+}
