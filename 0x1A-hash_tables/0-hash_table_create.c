bb80a68e67968383747ee04a9cea60d16a16c394
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-07 03:03:17 -0700

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+* hash_table_create - creates a hash table
+* @size: size of the hash table
+*
+* Return: newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	return (table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,9 @@
+__Contains the following project tasks and files:__
+* 0-hash_table_create.c  
+* 1-djb2.c  
+* 2-key_index.c  
+* 3-hash_table_set.c  
+* 4-hash_table_get.c  
+* 5-hash_table_print.c  
+* 6-hash_table_delete.c  
+* hash_tables.h  
