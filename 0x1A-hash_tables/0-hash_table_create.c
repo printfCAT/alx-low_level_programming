107a07f585006166d373701adf5de2eb6c5e0191
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-07 03:31:25 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = NULL;
+	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (!table->array)
+		return (NULL);
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 	return (table);
 }
