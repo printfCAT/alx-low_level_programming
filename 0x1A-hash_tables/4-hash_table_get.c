6a46773031e711f3e6edbfe7f62458187cf15edc
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-07 14:41:25 -0700

    search hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+* hash_table_get - retrieves value associated with a key
+* @ht: the hash table
+* @key: the key we are to retrive its value
+*
+* Return: the value of the key. NULL if unsuccessful
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+
+	if (ht || key)
+	{
+		index = hash_djb2((unsigned char *)key) % ht->size;
+		if (ht->array[index])
+		{
+			if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+		}
+	}
+	return (NULL);
+}
