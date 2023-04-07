38ae44a291673a40adccf6cdee370a3d45de9408
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-07 16:26:26 -0700

    print table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,19 +7,28 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int i;
+	unsigned long int i;
 	hash_node_t *current_node;
+	unsigned char comma_print = 1;
 
 	if (!ht)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		current_node = ht->array[i];
-		while (current_node)
+		if (ht->array[i])
 		{
-			printf("'%s' : '%s', ", current_node->key, current_node->value);
-			current_node = current_node->next;
+			if (comma_print == 0)
+				printf(", ");
+			current_node = ht->array[i];
+			while (current_node)
+			{
+				printf("'%s' : '%s'", current_node->key, current_node->value);
+				current_node = current_node->next;
+				if (current_node)
+					printf(", ");
+			}
+			comma_print = 0;
 		}
 	}
 	printf("}\n");
