bf33985f0930274be6176828513ba6a63f844de7
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-07 14:54:50 -0700

    search hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,12 +9,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
+	hash_node_t *current_item;
 
-	index = hash_djb2((unsigned char *)key) % ht->size;
-	if (ht->array[index])
+	if (ht && key)
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
-			return (ht->array[index]->value);
+		current_item = ht->array[index];
+		index = hash_djb2((unsigned char *)key) % ht->size;
+		while (current_item)
+		{
+			if (strcmp(current_item->key, key) == 0)
+				return (current_item->value);
+			current_item = current_item->next;
+		}
 	}
 	return (NULL);
 }
