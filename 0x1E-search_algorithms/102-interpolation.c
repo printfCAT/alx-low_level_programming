7525651fa897c675e76db506da3a64c8fcfe7216
RIvAL-411 <cliffomanyo@gmail.com>
2023-07-14 05:24:51 -0700

    task 9 fix

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,13 +10,13 @@
 */
 int interpolation_search(int *array, size_t size, int value)
 {
-	int low = 0, high = size - 1;
+	int low = 0, high;
 	size_t pos;
 
 	if (!array)
 		return (-1);
 
-	while (low <= high && value >= array[low] && value <= array[high])
+	for (low = 0, high = size - 1; high >= low;)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 		 * (value - array[low]));
@@ -24,7 +24,7 @@ int interpolation_search(int *array, size_t size, int value)
 			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 		else
 		{
-			printf("Value checked array[%ld] is out of range", pos);
+			printf("Value checked array[%ld] is out of range\n", pos);
 			break;
 		}
 
